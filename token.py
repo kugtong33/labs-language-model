1907fc28cccf7cbecbf6888b3ad08398176f64a3
Arjay Sitoy <sitoy.arjay@gmail.com>
2025-10-31 21:50:21 +0800

    feat(token): tokenizer with utf-8 encoding

--- token.py
+++ token.py
@@ -3,5 +3,10 @@ input = "hello world!"
 def tokenize(text):
     return [ord(char) for char in text]
 
-
 print(tokenize(input))
+
+
+def tokenizeutf(text):
+    return list(text.encode('utf-8'))
+
+print(tokenizeutf(input))
